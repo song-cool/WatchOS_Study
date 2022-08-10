9c8432f8c195169de51080033c61f22337ecd4e2
song-cool <oscar2978@naver.com>
2022-08-10 16:12:02 +0900

    [FEAT] Init WatchWeatherApp

---
+++ WatchWeather/WatchWeather WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchWeather WatchKit Extension
+//
+//  Created by 송시원 on 2022/08/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchWeather/WatchWeather WatchKit Extension/WatchWeatherApp.swift
@@ -0,0 +1,19 @@
+//
+//  WatchWeatherApp.swift
+//  WatchWeather WatchKit Extension
+//
+//  Created by 송시원 on 2022/08/10.
+//
+
+import SwiftUI
+
+@main
+struct WatchWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
